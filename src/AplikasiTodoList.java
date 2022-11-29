9205e452d44f0e2906d7b9413b6ba4848ae877c7
KangSDhi <kangteknisi@gmail.com>
2022-11-29 12:58:19 +0700

    Test Menampilkan Todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -3,7 +3,7 @@ public class AplikasiTodoList {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-
+        testShowTodoList();
     }
 
     /**
@@ -15,9 +15,15 @@ public class AplikasiTodoList {
             var no = i + 1;
 
             if (todo != null){
-                System.out.println(no + "." + todo);
+                System.out.println(no + ". " + todo);
             }
         }
+    }
+
+    public static void testShowTodoList(){
+        model[0] = "Belajar Java Dasar";
+        model[1] = "Studi Kasus Java Dasar : Aplikasi Todolist";
+        showTodoList();
     }
 
     /**
