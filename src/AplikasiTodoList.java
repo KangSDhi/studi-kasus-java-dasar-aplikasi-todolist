df660fedbef8fa96b8f18d49901e44ebf9bab290
KangSDhi <kangteknisi@gmail.com>
2022-11-30 11:00:46 +0700

    Test View Menambah Todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -5,7 +5,7 @@ public class AplikasiTodoList {
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
 
     public static void main(String[] args) {
-        testViewShowTodoList();
+        testViewAddTodoList();
     }
 
     /**
@@ -176,6 +176,19 @@ public class AplikasiTodoList {
         } else {
             addTodoList(todo);
         }
+    }
+
+    public static void testViewAddTodoList(){
+
+        addTodoList("Satu");
+        addTodoList("Dua");
+        addTodoList("Tiga");
+        addTodoList("Empat");
+        addTodoList("Lima");
+
+        viewAddTodoList();
+
+        showTodoList();
     }
 
     /**
