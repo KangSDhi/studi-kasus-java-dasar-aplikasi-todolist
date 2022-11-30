a1b0eb75f534fc714bbf8d5c9a0365db3a3f03a8
KangSDhi <kangteknisi@gmail.com>
2022-11-30 07:32:37 +0700

    Menambah Todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -29,8 +29,36 @@ public class AplikasiTodoList {
     /**
      * Menambah todo ke list
      */
-    public static void addTodoList(){
+    public static void addTodoList(String todo){
 
+        // cek apakah model penuh?
+        var isFull = true;
+        for (int i = 0; i < model.length; i++){
+            if (model[i] == null){
+                // model masih ada yang kosong
+                isFull = false;
+                break;
+            }
+        }
+
+        // jika penuh, kita resize ukuran array 2x lipat
+        if (isFull){
+            // data model lama disimpan di dalam temp
+            var temp = model;
+            model = new String[model.length * 2];
+
+            for (int i = 0; i < temp.length; i++){
+                model[i] = temp[i];
+            }
+        }
+
+        // tambahkan ke posisi yang data array nya NULL
+        for (var i = 0; i < model.length; i++){
+            if (model[i] == null){
+                model[i] = todo;
+                break;
+            }
+        }
     }
 
     /**
